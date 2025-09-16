e9cd1417cdf2602b0cea668e319d2aa91c7f1195
LazycatLliox <cte-atreyu.blum@sllboces.org>
2025-09-16 13:52:05 +0000

    hi

---
+++ PrintExpressions.py
@@ -0,0 +1,15 @@
+'''
+Class: CIS Seniors 25-26
+Name: Atreyu Blum
+Assignment: Printing Expressions
+'''
+
+print("Hello, My name is Atreyu")
+
+print("My favorite song!")
+print("--------------------")
+print("")
+print("I was a ghost, I was alone")
+print("어두워진 앞길 속에 (ah)")
+print("Given the throne, I didn't know how to believe")
+print("I was the queen that I'm meant to be")
