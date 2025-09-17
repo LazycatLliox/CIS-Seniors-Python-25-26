25216890072b31bbf1953cb9cd5decdc341ffc82
LazycatLliox <cte-atreyu.blum@sllboces.org>
2025-09-17 14:18:44 +0000

    Variables

---
+++ variables.py
@@ -0,0 +1,36 @@
+'''
+File name: variables
+Name: Atreyu Blum
+Date: 09/17/2025
+
+Practicing variables basics 
+'''
+
+#Variables can hold all data types
+myInt = 42
+myFloat = 3.1415
+myString = "Atreyu"
+myBool = True
+
+print("My name is:" , myString)
+
+myAge = myInt
+
+print("my age is:", myAge)
+print("I do not want to be",myInt + myAge)
+
+print(myFloat)
+myFloat = 6.28210
+print(myFloat)
+
+print("\n\n\n")
+print("My ticket App")
+print("-----------------")
+ticketTotal = 43.50 + 43.50 + 43.50
+processFee = 2.95
+venueFee = 3.95 + 3.95 + 3.95
+
+print("\nMy subtotal is:")
+print("-----------------")
+subtotal = ticketTotal + processFee + venueFee
+print("$",subtotal)
