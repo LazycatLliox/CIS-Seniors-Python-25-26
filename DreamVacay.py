bfadb0591238f4840770fd306ef24b7e0fcb0497
LazycatLliox <cte-atreyu.blum@sllboces.org>
2025-09-18 13:27:28 +0000

    dreamVacay

---
+++ DreamVacay.py
@@ -0,0 +1,21 @@
+'''
+File: DreamVacay
+Name: Atreyu Blum
+Date: 09/18/2025
+Course: CIS Seniors 25-26
+'''
+
+#Variables for Dream Vacation
+destination = "Japan"
+duration = "14" 
+cost = "10000"
+activities = "be buying way to much anime merch ,sightseeing, sushi tasting, and visiting historic temples"
+travleCompanions = "Jonathan and Elijah "
+
+#printing the details of the dream vacation
+print("\n")
+print("My dream vacation is to go to " + destination + ".")
+print("I would like to stay for " + duration + " days.")
+print("I think the trip would cost around $" + cost + ".")
+print("While Im there I would " + activities + ".")
+print("I would like to go with " + travleCompanions + ".")
