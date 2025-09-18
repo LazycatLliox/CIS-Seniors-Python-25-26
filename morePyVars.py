4fa5c1eb9eb65c8ac6d81ae0c7db3c0642eeb62d
LazycatLliox <cte-atreyu.blum@sllboces.org>
2025-09-18 13:17:37 +0000

    moreVars

---
+++ morePyVars.py
@@ -0,0 +1,39 @@
+'''
+The Doc String
+Notes about python variables.
+Name: Atreyu Blum
+Date: 09/18/2025
+'''
+
+#Variable assignment
+print("\n\n\n")
+firstName = "Atreyu"
+lastName = "Blum"
+grade = "12"
+favoriteSubject = "Computer Science"
+yearsOfSchool = "Too many years"
+
+print("Hello, my name is" + firstName + " " + lastName + ".")
+print("I am in grade" + grade + "th grade.")
+print("My favorite subject is " + favoriteSubject + ".")
+print("I have been in school for " + yearsOfSchool + ".")
+
+print("\n\n\n")
+
+print("Mathmatical Calculations")
+
+#Storing mathmatical values
+length = 15 
+width = 8 
+height = 12
+
+#claculating area and volume
+area = length * width
+volume = length * width * height
+
+#print the values 
+print("The area of the structure is:", area)
+print("The volume of the structure is:", volume)
+
+print("\n\n\n")
+
