8730668ed1c743528c6efc65db127acd116048c7
LazycatLliox <cte-atreyu.blum@sllboces.org>
2025-09-18 14:33:42 +0000

    SocialMedia

---
+++ socialMediaProfile.py
@@ -0,0 +1,31 @@
+'''
+File Name: socialMediaProfile.py
+Name: Atreyu Blum
+Course Name: CIS Seinors 25-26
+Date: 9/18/2025
+'''
+
+# Social Media Variables
+username = "Frieren" 
+bio = "I hate demons "
+quote = ["We traveled together for a mere ten years. I knew human lives were short, but why didn't I try to get to know him better?"]
+posts = 150
+followers = 1200
+following = 300
+is_verified = True
+is_private = False
+avearage_likes = 250.75
+engagment_rate = 4.5
+
+# Print Social Media Profile Information
+print("Username:", username)
+print("Bio:", bio)
+print("Quotes:", quote)
+print("Posts:", posts)
+print("Followers:", followers)
+print("Following:", following)
+print("Verified Account:", is_verified)
+print("Private Account:", is_private)
+print("Average Likes per Post:", avearage_likes)
+print("Engagement Rate (%):", engagment_rate)
+
