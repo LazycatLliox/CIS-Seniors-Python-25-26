4a7673212a421aef61e317ec4db14584911706c3
LazycatLliox <cte-atreyu.blum@sllboces.org>
2025-11-06 15:04:21 +0000

    11/6/25

--- gradeCalc.py
+++ gradeCalc.py
@@ -6,7 +6,7 @@ exam3_grade = float(input("Enter score on Exam 3 (out of 100): "))
 
 examAvg = (exam1_grade + exam2_grade + exam3_grade) / 3
 
-print(f"Your overall grade is: {examAvg:.2f}%")
+
 
 print("\n\nStudent versions:")
 
@@ -29,7 +29,7 @@ grade4 = calculate_grade(program4)
 
 progAvg = ((grade1 + grade2 + grade3 + grade4) / 4) * 100
 
-print(f"\nStudent overall programming assignment grade is: {progAvg:.2f}%")
+
 # Calculates the overall grade for four equally weighted programming assignments, in which assignments 1 and 2 are graded out of 50 points and assignments 3 and 4 are graded out of 75 points.
 assign1 = int(input("Enter the score you got on programming assignment 1: "))
 assign2 = int(input("Enter the score you got on programming assignment 2: "))
@@ -41,12 +41,15 @@ assignGrade2 = calculate_grade(assign2)
 assignGrade3 = calculate_grade(assign3)
 assignGrade4 = calculate_grade(assign4)
 
-progAvg = ((assignGrade1 + assignGrade2 + assignGrade3 + assignGrade4) / 4) * 100
+assignAvg = ((assignGrade1 + assignGrade2 + assignGrade3 + assignGrade4) / 4) * 100
 
-print(f"\nStudent overall programming assignment grade is: {progAvg:.2f}%")
 # Calculates the overall grade for a course with three equally weighted exams (graded out of 100 points) that account for 60% of the overall grade and four equally weighted programming assignments (graded out of 50 points) that account for 40% of the overall grade. Hint: The overall grade can be calculated as 0.6 * averageExamScore + 0.4 * averageProgScore.
 print("\n Section 3:")
 
-overallAvg = (0.6 * examAvg) + (0.4 * progAvg)
-print(f"\nStudent overall Average is: {overallAvg:.2f}%")
+overallAvg = (0.5 * examAvg) + (0.4 * progAvg) + (0.1 * assignAvg)
+
+print(f"\nStudent's overall exam average is: {examAvg:.2f}%")
+print(f"\nStudent's overall programming programing average is: {progAvg:.2f}%")
+print(f"\nStudent's overall assignment average is: {assignAvg:.2f}%")
+print(f"\nStudent's overall average is: {overallAvg:.2f}%")
 
